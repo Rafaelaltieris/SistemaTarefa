3c4aeff60f291ae6a27e95d60721b9424661286b
Rafaelaltieris <raphaelaltieris@gmail.com>
2025-09-05 11:37:43 -0300

    Add Token

--- SistemaTarefa/Controllers/ContaController.cs
+++ SistemaTarefa/Controllers/ContaController.cs
@@ -1,7 +1,6 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using SistemaTarefa.Data;
